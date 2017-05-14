5ab0c0b0fe84bdba1b128d621303b149972b3765
gravoo <gravoo@yahoo.com>
2017-05-15 00:18:32 +0200

    zadanie 2 rozwiazane

--- Scheme/zad2.scm
+++ Scheme/zad2.scm
@@ -3,53 +3,65 @@
 ; (deriv '(x / b) 'x) 
 ; (deriv '(x * b) 'x) 
 ; (deriv '((x + a) * (x + b)) 'x)
-(define (deriv exp var)
+(define (pochodna wyrazenie zmienna)
+  list (liczpochodna (upraszczacz wyrazenie) zmienna))
+
+(define (dupa)
+  (list 5 5 '()))
+
+(define (liczpochodna wyrazenie zmienna)
+  (cond ((number? wyrazenie) 0)
+        ((symbol? wyrazenie)
+         (if (eqv? wyrazenie zmienna) 1 0))
+        ((null? (cdr wyrazenie)) (liczpochodna (car wyrazenie) zmienna))
+        (else            
+          (let ((operand1 (car  wyrazenie))
+                (operator (cadr wyrazenie))
+                (operands (cddr wyrazenie)))
+            (case operator
+               ((+) (list (liczpochodna operand1 zmienna) '+ (liczpochodna operands zmienna)))
+               ((*) (list (list (liczpochodna operand1 zmienna) '* (caddr wyrazenie) ) '+
+                          (list  operand1 '* (liczpochodna operands zmienna))))
+            )
+        )))
+ )
+
+(define (upraszczacz exp)
   (cond
     ((or (symbol? exp)(number? exp)) exp)
     ((operation exp '+)
-     (make-sum (deriv (car exp) var)
-                          (deriv (caddr exp) var)))
+     (make-sum (upraszczacz (car exp))
+                          (upraszczacz (caddr exp))))
     ((operation exp '-)
-        (make-sub (deriv (car exp) var) (deriv (caddr exp) var)))
+        (make-sub (upraszczacz (car exp)) (upraszczacz (caddr exp))))
     ((operation exp '*)
-        (make-product (deriv (car exp) var) (deriv (caddr exp) var)))
+        (make-product (upraszczacz (car exp)) (upraszczacz (caddr exp))))
     ((operation exp '/)
-        (make-div (deriv (car exp) var) (deriv (caddr exp) var)))
+        (make-div (upraszczacz (car exp)) (upraszczacz (caddr exp))))
     ))
 
 (define (operation exp operand)
   (and (pair? exp) (eqv? operand (cadr exp))))
 (define (make-sum a b)
-  (cond ((and(number? a)(number? b) (+ a b)))
+  (cond 
+    ((and(number? a)(number? b) (+ a b)))
         (else (list a '+ b))))
 (define (make-product a b)
-  (cond ((and(number? a)(number? b) (* a b)))
+  (cond 
+    ((and(number? a)(number? b) (* a b)))
         (else (list a '* b))))
 (define (make-sub a b)
-  (cond ((and(number? a)(number? b) (- a b)))
+  (cond 
+    ((and(number? a)(number? b) (- a b)))
         (else (list a '- b))))
 (define (make-div a b)
-  (cond ((and(number? a)(number? b) (/ a b)))
+  (cond 
+    ((and(number? a)(number? b) (/ a b)))
         (else (list a '/ b))))
 
-(trace deriv)
+(trace upraszczacz)
 (trace make-sum)
 (trace sum?)
-
-(define (pochodna wyrazenie)
-  (cond
-    ((null? wyrazenie) 0)
-    ((symbol? wyrazenie) 'wyrazenie)
-    ((null? (cdr wyrazenie)) (car wyrazenie))
-    (else
-      (let ((skladnik (car wyrazenie))
-            (operator (cadr wyrazenie))
-            (reszta (cddr wyrazenie)))
-        ((case operator
-           ((+) +))
-        skladnik
-        (pochodna reszta))
-       ))
-    ))
 (trace pochodna)
+(trace liczpochodna)
 
