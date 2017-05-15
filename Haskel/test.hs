692ae91e2d23adcf108c468c39117fe6d0fdb7ee
gravoo <gravoo@yahoo.com>
2017-05-16 00:42:41 +0200

    pierwsze zadanie w haskell

---
+++ Haskel/test.hs
@@ -0,0 +1,2 @@
+doubleMe x = x + x
+lista = [1,2,3,4,5]
---
+++ Haskel/zad1.hs
@@ -0,0 +1,7 @@
+-- scalarproduct [1,2,3] [3,2,1]
+-- scalarproduct [1,0] [0,1]
+-- scalarproduct [1,2] [3,4]
+--  
+scalarproduct a b = (scalarproduct' (zip a b))
+scalarproduct' a = sum [ x * y | (x, y) <- a]
+
